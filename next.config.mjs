a6d02b3cc6633527d98fdf03628dff7f0c3eaf33
Abdullah7175 <abdullah7175@gmail.com>
2025-06-28 15:34:26 +0500

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -7,20 +7,12 @@ const nextConfig = {
     JWT_SECRET: process.env.JWT_SECRET,
   },
   images: {
-    domains: ['placehold.co', 'localhost', 'vercel.app', 'vercel.com'],
+    domains: ['placehold.co','localhost'],
     dangerouslyAllowSVG: true,
   },
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        'localhost:3000', 
-        'localhost:3001', 
-        'localhost:3002', 
-        'localhost:3003', 
-        'localhost:3004',
-        '*.vercel.app',
-        '*.vercel.com'
-      ],
+      allowedOrigins: ['localhost:3000', 'localhost:3001', 'localhost:3002', 'localhost:3003', 'localhost:3004'],
     },
   },
   serverExternalPackages: ['sharp', 'fs-extra'],
